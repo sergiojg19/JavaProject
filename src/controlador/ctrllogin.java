e7d4ce74176dd9a54ee99dd76b7fbe3dc9d38728
sergiojg19 <sergio.jumbo@unl.edu.ec>
2023-02-22 09:14:17 -0500

    configurar loading

--- src/controlador/ctrllogin.java
+++ src/controlador/ctrllogin.java
@@ -24,6 +24,7 @@ public class ctrllogin implements ActionListener {
     frmlogin frmlogin = new frmlogin();
     ctrlconsultas ctrlconsultas = new ctrlconsultas();
     Hasher hasher = new Hasher();
+    boolean estaAutenticado = false;
       // modelo.addRow(new Object[]{nombre});
     public ctrllogin() {
         frmlogin.btnIniciaSesion.addActionListener(this);
@@ -31,6 +32,7 @@ public class ctrllogin implements ActionListener {
 //        frmlogin.btnsalir.addActionListener(this);
         frmlogin.setVisible(true);
         frmlogin.setLocationRelativeTo(null);
+        frmlogin.loadingBar.setVisible(false);
     }
     private boolean validarCampos(){
         String username = frmlogin.txtusername.getText();
@@ -41,7 +43,7 @@ public class ctrllogin implements ActionListener {
         return true;
     }
     private Usuarios autenticarUsuario(){
-        iniciarCarga();
+        estaAutenticado = false;
         String username = frmlogin.txtusername.getText();
         String password = new String(frmlogin.txtpassword.getPassword());
         List listado = ctrlconsultas.obtenerListadoUsuarios();
@@ -49,6 +51,7 @@ public class ctrllogin implements ActionListener {
         while(it.hasNext()){
             Usuarios usuario = (Usuarios) it.next();
             if(username.equals(usuario.getUsername()) && hasher.checkPassword(password, usuario.getPassword())){
+                estaAutenticado = true;
                 return usuario;
             };
         }
@@ -79,12 +82,14 @@ public class ctrllogin implements ActionListener {
                 for (int i = 1; i <= 100; i++) {
                     try {
                         frmlogin.loadingBar.setValue(i);
-                        sleep(15);
+                        sleep(9);
                     } catch (InterruptedException e) {
                         //Logger.getLogger(ctrlloading.class.getName()).log(Level.ERROR, null, e);
                         System.out.println("ERROR "+e.getMessage());
                     }
-                    
+                    if(frmlogin.loadingBar.getValue()==99){
+                        cargarSistema();
+                    }
                 }
             }
         };
@@ -93,20 +98,23 @@ public class ctrllogin implements ActionListener {
     public void cerrarCarga(){
         frmlogin.loadingBar.setVisible(false);
     }
-    
+    public void cargarSistema(){
+        Usuarios usuario = autenticarUsuario();
+        if(usuario !=null){
+            ctrlsystem ctrlsystem = new ctrlsystem(getEstudianteSegunUsuario(usuario));
+            frmlogin.dispose();
+        }else{
+            JOptionPane.showMessageDialog(null, "Usuario o contraseña incorrectos.", "ERROR_MESSAGE", JOptionPane.ERROR_MESSAGE);
+        }
+    }
     
      @Override
     public void actionPerformed(ActionEvent e) {
 
         if (frmlogin.btnIniciaSesion == e.getSource()) {
             if (validarCampos()){
-                Usuarios usuario = autenticarUsuario();
-                if(usuario !=null){
-                    ctrlsystem ctrlsystem = new ctrlsystem(getEstudianteSegunUsuario(usuario));
-                    frmlogin.dispose();
-                }else{
-                    JOptionPane.showMessageDialog(null, "Usuario o contraseña incorrectos.", "ERROR_MESSAGE", JOptionPane.ERROR_MESSAGE);
-                }
+                iniciarCarga();
+                //cargarSistema();
             }else{
              JOptionPane.showMessageDialog(null, "Existen campos por completar o corregir", "WARNING_MESSAGE", JOptionPane.WARNING_MESSAGE);
             }
